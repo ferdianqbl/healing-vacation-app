07f4270e5fc18c183836585c5b34bd7b3e79d07e
ferdianqbl <muhammadferdianiqbal@gmail.com>
2022-10-04 19:33:03 +0700

    add accordion FAQ

--- src/components/Faq/Faq.jsx
+++ src/components/Faq/Faq.jsx
@@ -18,10 +18,7 @@ const Faq = () => {
           </div>
 
           <div className="col-md-6">
-            <div
-              className="accordion accordion-flush"
-              id="accordionFlushExample"
-            >
+            <div className="accordion accordion-flush" id="faq-accordion">
               <div className="accordion-item">
                 <h2 className="accordion-header" id="flush-headingOne">
                   <button
@@ -32,19 +29,18 @@ const Faq = () => {
                     aria-expanded="false"
                     aria-controls="flush-collapseOne"
                   >
-                    Accordion Item #1
+                    Healing itu apa sih ?
                   </button>
                 </h2>
                 <div
                   id="flush-collapseOne"
                   className="accordion-collapse collapse"
                   aria-labelledby="flush-headingOne"
-                  data-bs-parent="#accordionFlushExample"
+                  data-bs-parent="#faq-accordion"
                 >
                   <div className="accordion-body">
-                    Placeholder content for this accordion, which is intended to
-                    demonstrate the <code>.accordion-flush</code> className.
-                    This is the first item's accordion body.
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Turpis ut varius adipiscing condimentum.
                   </div>
                 </div>
               </div>
@@ -58,20 +54,18 @@ const Faq = () => {
                     aria-expanded="false"
                     aria-controls="flush-collapseTwo"
                   >
-                    Accordion Item #2
+                    Healing dibuat sama siapa ya?
                   </button>
                 </h2>
                 <div
                   id="flush-collapseTwo"
                   className="accordion-collapse collapse"
                   aria-labelledby="flush-headingTwo"
-                  data-bs-parent="#accordionFlushExample"
+                  data-bs-parent="#faq-accordion"
                 >
                   <div className="accordion-body">
-                    Placeholder content for this accordion, which is intended to
-                    demonstrate the <code>.accordion-flush</code> className.
-                    This is the second item's accordion body. Let's imagine this
-                    being filled with some actual content.
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Turpis ut varius adipiscing condimentum.
                   </div>
                 </div>
               </div>
@@ -85,23 +79,68 @@ const Faq = () => {
                     aria-expanded="false"
                     aria-controls="flush-collapseThree"
                   >
-                    Accordion Item #3
+                    Fitur sementara apa saja?
                   </button>
                 </h2>
                 <div
                   id="flush-collapseThree"
                   className="accordion-collapse collapse"
                   aria-labelledby="flush-headingThree"
-                  data-bs-parent="#accordionFlushExample"
+                  data-bs-parent="#faq-accordion"
                 >
                   <div className="accordion-body">
-                    Placeholder content for this accordion, which is intended to
-                    demonstrate the <code>.accordion-flush</code> className.
-                    This is the third item's accordion body. Nothing more
-                    exciting happening here in terms of content, but just
-                    filling up the space to make it look, at least at first
-                    glance, a bit more representative of how this would look in
-                    a real-world application.
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Turpis ut varius adipiscing condimentum.
+                  </div>
+                </div>
+              </div>
+              <div className="accordion-item">
+                <h2 className="accordion-header" id="flush-headingFour">
+                  <button
+                    className="accordion-button collapsed"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#flush-collapseFour"
+                    aria-expanded="false"
+                    aria-controls="flush-collapseFour"
+                  >
+                    Bagaimana cakupan wisatanya?
+                  </button>
+                </h2>
+                <div
+                  id="flush-collapseFour"
+                  className="accordion-collapse collapse"
+                  aria-labelledby="flush-headingFour"
+                  data-bs-parent="#faq-accordion"
+                >
+                  <div className="accordion-body">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Turpis ut varius adipiscing condimentum.
+                  </div>
+                </div>
+              </div>
+              <div className="accordion-item">
+                <h2 className="accordion-header" id="flush-headingFive">
+                  <button
+                    className="accordion-button collapsed"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#flush-collapseFive"
+                    aria-expanded="false"
+                    aria-controls="flush-collapseFive"
+                  >
+                    Bagaimana prospeknya?
+                  </button>
+                </h2>
+                <div
+                  id="flush-collapseFive"
+                  className="accordion-collapse collapse"
+                  aria-labelledby="flush-headingFive"
+                  data-bs-parent="#faq-accordion"
+                >
+                  <div className="accordion-body">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Turpis ut varius adipiscing condimentum.
                   </div>
                 </div>
               </div>
