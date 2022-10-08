d6c1d18cb701165746e04b9bb15df5dd8bd99146
ferdianqbl <muhammadferdianiqbal@gmail.com>
2022-10-08 10:33:48 +0700

    create step condition

--- src/Parts/Step/Step.jsx
+++ src/Parts/Step/Step.jsx
@@ -5,6 +5,12 @@ import StepImg from "./step.png";
 import stepJson from "../../json/step.json";
 
 const Step = () => {
+  const [step, setStep] = React.useState(1);
+
+  const stepHandler = (numberOfStep) => {
+    setStep(numberOfStep);
+  };
+
   return (
     <section className="step" id="step">
       <div className="container">
@@ -16,10 +22,19 @@ const Step = () => {
                 className="step-item"
                 key={new Date() + index * 100000 * 999}
               >
-                <div className="step-item-number">
+                <div
+                  className={`step-item-number${
+                    step === item.step ? " active" : ""
+                  }`}
+                  onClick={() => stepHandler(item.step)}
+                >
                   <span>{item.step}.</span>
                 </div>
-                <div className="step-item-text">
+                <div
+                  className={`step-item-text${
+                    step === item.step ? " d-block" : " d-none"
+                  }`}
+                >
                   <p className="step-item-title">{item.title}</p>
                   <p className="step-item-body">{item.body}</p>
                 </div>
